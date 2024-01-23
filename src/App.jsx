14f8032d3ce0480da91a02a878fa8cd732b6738d
Twanivi <aikitashoo@gmail.com>
2024-01-23 21:34:34 +0200

    correct hw4-5

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,48 @@
-import './App.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { UserData } from './components/UserData/UserData';
-import {OnLoadingUserData} from './components/OnLoadingPersonsData/OnLoadingPersonsData';
-import { FormUsers } from './components/FormUsers/FormUsers';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { UserData } from "./components/UserData/UserData";
+import { FormUsers } from "./components/FormUsers/FormUsers";
 
 function App() {
+  const [users, setUsers] = useState({
+    id: '',
+    name: '',
+    username: '',
+    email: '',
+    address: {
+      city: '',
+      street: '',
+    },
+  });
 
-  const DataLoading = OnLoadingUserData(UserData);
+  const [appState, setAppState] = useState({
+    loading: false,
+    persons: null,
+  });
 
-  const [appState, setAppState] = useState(
-    {
-      loading: false,
-      persons: null
-    }
-  );
-  
   useEffect(() => {
-    setAppState({loading: true})
-    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+    setAppState({ loading: true });
+    const apiUrl = "https://jsonplaceholder.typicode.com/users";
     axios.get(apiUrl).then((resp) => {
       const allPersons = resp.data;
       setAppState({
         loading: false,
-        persons: allPersons
+        persons: allPersons,
       });
     });
   }, [setAppState]);
 
-  const [formInputData, setFormInputData] = useState({
-    name: '',
-    username: '',
-    email: '',
-    address: ''
-  })
-
   return (
-    <div className='app'>
-      <FormUsers  setFormInputData={setFormInputData}/>
-      <DataLoading isLoading={appState.loading} persons={appState.persons} formInputData={formInputData}/>
+    <div className="app">
+      <FormUsers users={users} setUsers={setUsers} />
+      {appState.loading ? (
+        <h1>Подождите, данные загружаются!</h1>
+      ) : (
+        <UserData persons={appState.persons} users={users} />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/FormUsers/FormUsers.jsx
+++ src/components/FormUsers/FormUsers.jsx
@@ -1,40 +1,76 @@
+import "./FormUsers.css";
 import { useState } from "react";
 
-export const FormUsers = ({setFormInputData}) => {
-    const [profileData, setProfileData] = useState({
-        name: '',
-        username: '',
-        email: '',
-        address: ''
-    })
+export const FormUsers = ({users, setUsers}) => {
+  const [userId, setUserId] = useState(10);
+  const [userName, setUserName] = useState("");
+  const [userNik, setUserNik] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [addressCity, setAddressCity] = useState("");
+  const [addressStreet, setAddressStreet] = useState("");
 
-    const handleCreateNewProfile = () => {
-        setFormInputData(profileData);
+  const addUser = (newUser) => {
+    setUsers([...users, newUser])
+  }
+
+  const addNewUser = (event) => {
+    event.preventDefault();
+    setUserId(prevState => ++prevState);
+    const newUser = {
+      id: userId,
+        name: userName,
+        username: userNik,
+        email: userEmail,
+        address: {
+          city: addressCity,
+          street: addressStreet,
+        }
     }
+    addUser(newUser);
+    setUserName("");
+    setUserNik("");
+    setUserEmail("");
+    setAddressCity(""); 
+    setAddressStreet("");
+  };
 
-    const handleSetData = (data) => {
-        setProfileData(prevState => ({
-            ...prevState,
-            ...data
-        }))
-    }
+  const handleChangeName = (event) => {
+    setUserName(event.target.value);
+  };
 
-    return (
+  const handleChangeNik = (event) => {
+    setUserNik(event.target.value);
+  };
+
+  const handleChangeEmail = (event) => {
+    setUserEmail(event.target.value);
+  };
+
+  const handleChangeCity = (event) => {
+    setAddressCity(event.target.value);
+  };
+
+  const handleChangeStreet = (event) => {
+    setAddressStreet(event.target.value);
+  };
+
+  return (
+    <div className='form-container'>
+      <form onSubmit={addNewUser} className="form-user">
+          <p>Name:</p>
+          <input value={userName} onChange={handleChangeName} />
+          <p>Nik:</p>
+          <input value={userNik} onChange={handleChangeNik} />
+          <p>Email:</p>
+          <input value={userEmail} onChange={handleChangeEmail} />
+          <p>City:</p>
+            <input value={addressCity} onChange={handleChangeCity} />
+            <p>Street:</p>
+            <input value={addressStreet} onChange={handleChangeStreet} />
         <div>
-            <p>Name:</p>
-            <input value={profileData.name} onChange={event => handleSetData({name: event.target.value})} />
-            <p>Nik:</p>
-            <input value={profileData.username} onChange={event => handleSetData({username: event.target.value})} />
-            <p>Email:</p>
-            <input value={profileData.email} onChange={event => handleSetData({email: event.target.value})} />
-            <p>Address:</p>
-            <input value={profileData.address} onChange={event => handleSetData({address: event.target.value})} />
-            <div>
-                <button onClick={handleCreateNewProfile}>
-                    Create new Profile
-                </button>
-            </div>
+          <button>Create new Profile</button>
         </div>
-    )
-
-}
+      </form>
+    </div>
+  );
+};
